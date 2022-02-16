a3cd25dd2e5ebeb36e8237dd7202070eb5231d62
sannypro <sannymr79@gmail.com>
2022-02-17 00:14:37 +0600

    error handling done

--- js/app.js
+++ js/app.js
@@ -2,17 +2,59 @@ document.getElementById('expanse-calculate').addEventListener('click', function 
     const foodExpanse = document.getElementById('food-expanse');
     const rentExpanse = document.getElementById('rent-expanse');
     const clothesExpanse = document.getElementById('clothes-expanse');
-    const foodExpanseInt = parseInt(foodExpanse.value);
-    const rentExpanseInt = parseInt(rentExpanse.value);
-    const clothesExpanseInt = parseInt(clothesExpanse.value);
-    const totalExpanse = foodExpanseInt + rentExpanseInt + clothesExpanseInt;
-    const showTotaExpanse = document.getElementById('total-expanse')
-    showTotaExpanse.innerText = totalExpanse;
-    const income = document.getElementById('income');
-    const BalanceCalculation = income.value - totalExpanse
-    const totalBalance = document.getElementById('balance');
-    totalBalance.innerText = BalanceCalculation;
+    if (foodExpanse.value == '') {
+        alert('if you do not want to fill any box simply type 0')
+    }
+    else if (parseInt(foodExpanse.value) < 0) {
+        alert("please insert positive amount")
+    }
+    else {
+        const foodExpanseInt = parseInt(foodExpanse.value);
+        if (rentExpanse.value == '') {
+            alert('if you do not want to fill any box simply type 0')
 
+        }
+        else if (parseInt(rentExpanse.value) < 0) {
+            alert("please insert positive amount")
+        }
+        else {
+            debugger;
+            if (clothesExpanse.value == '') {
+                alert('if you do not want to fill any box simply type 0')
+            }
+            else if (parseInt(clothesExpanse.value) < 0) {
+                alert("please insert positive amount")
+            }
+            else {
+                const rentExpanseInt = parseInt(rentExpanse.value);
+
+                const clothesExpanseInt = parseInt(clothesExpanse.value);
+                const totalExpanse = foodExpanseInt + rentExpanseInt + clothesExpanseInt;
+                const showTotaExpanse = document.getElementById('total-expanse')
+                showTotaExpanse.innerText = totalExpanse;
+                const income = document.getElementById('income');
+                if (income.value < 0) {
+                    alert("please insert positive amount");
+                    showTotaExpanse.innerText = '0'
+                }
+                else if (income.value == '') {
+                    alert("you did not input income")
+                    showTotaExpanse.innerText = '0'
+                }
+                else {
+                    const BalanceCalculation = income.value - totalExpanse;
+                    const totalBalance = document.getElementById('balance');
+                    if (BalanceCalculation >= 0) {
+                        totalBalance.innerText = BalanceCalculation;
+                    }
+                    else {
+                        alert("you don't have enough money to expanse")
+                        showTotaExpanse.innerText = '0'
+                    }
+                }
+            }
+        }
+    }
 })
 
 document.getElementById('savings').addEventListener('click', function () {
@@ -24,6 +66,16 @@ document.getElementById('savings').addEventListener('click', function () {
     savedAmount.innerText = saving;
     const savingCalculation = parseFloat(totalBalance.innerText) - saving;
     const remainingBalance = document.getElementById('remaining-balance');
-    remainingBalance.innerText = savingCalculation;
+    if (savingInput.value < 0) {
+        alert("please insert positive amount");
+        savedAmount.innerText = '0';
+    }
+    else if (savingInput.value > 100) {
+        alert("you do not have enough money to save");
+        savedAmount.innerText = '0';
+    }
+    else {
+        remainingBalance.innerText = savingCalculation;
+    }
 
 })
